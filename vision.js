44f8883e077b6805473415fb03861948c06171b7
dogclassifier <75272150+dogclassifier@users.noreply.github.com>
2021-01-07 20:11:15 +0530

    Create vision.js

---
+++ vision.js
@@ -0,0 +1,33 @@
+function setup {
+                                                      
+                                    var options = {
+
+                                         video: {
+
+                                             facingMode: {
+                                              exact: "environment"
+                                            }
+
+                                         },
+                                    audio: false
+                                       };
+
+
+                                    w=window.innerWidth;
+                                    head=20;
+                                    h=window.innerHeight/2;
+                                                                        
+                                    cnv1=createCanvas(w,h);
+                                    cnv2=createCanvas(w,h);
+                                    cnv1.position(0, 0, 'relative');
+                                    cnv2.position(0,h,'relative'
+                                    camCapture = createCapture(options);
+                                    camCapture.size(w,h);
+                                    camCapture.hide();
+                                    }
+
+                                  
+                        function draw() {
+                              cnv1=image(0,0,w,h)
+                              
+                        }
